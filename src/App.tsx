be809fcc3460f0df55deb4a1230d266edcd2c289
Tomohiro Endo <europeanplaice@gmail.com>
2024-07-28 15:01:59 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <Authenticator>
-      {({signOut}) => {
+      {({signOut}) => (
         <main>
           <h1>My todos</h1>
           <button onClick={createTodo}>+ new</button>
@@ -44,7 +44,7 @@ function App() {
           </div>
           <button onClick = {signOut}>Sign out</button>
         </main>
-      }}
+      )}
     </Authenticator>
   );
 }
